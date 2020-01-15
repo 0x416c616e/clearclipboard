fc772858411cf0eb737c42028b61b3a09b7ae46b
Alan <29168822+0x416c616e@users.noreply.github.com>
2020-01-15 05:11:12 -0600

    Update background.js

--- background.js
+++ background.js
@@ -1,16 +1,5 @@
 function openPage() {
-  copy("this is a test");
-  alert("hello");
-  console.log("testing");
-  navigator.clipboard.writeText("<empty clipboard>").then(function() {
-  /* clipboard successfully set */
-    console.log("success");
-  }, function() {
-  /* clipboard write failed */
-    console.log("failure");
-  });
+  console.log("the popup specified in manifest.json does what used to be here");
 }
-
-
 
 browser.browserAction.onClicked.addListener(openPage);
